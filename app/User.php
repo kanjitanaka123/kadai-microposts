a873bb4aeb40bd380cde886d3fa150e76b159f6d
kanji.tanaka <phpkanji@gmail.com>
2020-05-28 12:50:43 +0000

    post

--- app/User.php
+++ app/User.php
@@ -9,6 +9,16 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+    
+        public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+    
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts');
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +46,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+
 }
